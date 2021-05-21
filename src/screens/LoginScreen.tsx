796dc6715f34479f89b79773c671b711d9588122
Filip <lidvanlol@gmail.com>
2021-05-21 15:21:33 +0200

    Update LoginScreen.tsx

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -8,8 +8,7 @@ function LoginScreen() {
   const { biometrics } = React.useContext(DeviceContext);
   const { setUser } = React.useContext(UserContext);
   const [useBiometricLogin, setUseBiometricLogin] = React.useState(biometrics);
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+ 
 
   React.useEffect(() => {
     console.log(useBiometricLogin);
@@ -20,7 +19,7 @@ function LoginScreen() {
     async function handleBiometricLogin() {
       const result: LocalAuthentication.LocalAuthenticationResult =
         await LocalAuthentication.authenticateAsync({
-          promptMessage: "Login to My Test App",
+          promptMessage: "Logujte se Otiskom Prsta",
         });
 
       if (result.success === true) {
@@ -31,43 +30,17 @@ function LoginScreen() {
     }
   }, [biometrics, useBiometricLogin, setUseBiometricLogin]);
 
-  function handleInputLogin() {
-    setUser({ username, password });
-  }
-
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 22, fontWeight: "700" }}>Login to App</Text>
+      <Text style={{ fontSize: 22, fontWeight: "700" }}>
+        Logujte se otiskom prsta
+      </Text>
       <View style={{ marginTop: 40, width: "70%" }}>
-        <TextInput
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-          placeholder="username"
-          style={{
-            backgroundColor: "#e2e2e2",
-            padding: 10,
-            borderRadius: 3,
-            fontSize: 16,
-            height: 44,
-          }}
-        />
         <View style={{ marginTop: 20, position: "relative", height: 44 }}>
-          <TextInput
-            placeholder="password"
-            onChangeText={(text) => setPassword(text)}
-            secureTextEntry
-            style={{
-              backgroundColor: "#e2e2e2",
-              padding: 10,
-              borderRadius: 3,
-              fontSize: 16,
-              height: 44,
-            }}
-          />
           {biometrics && (
             <TouchableOpacity
               onPress={() => setUseBiometricLogin(true)}
-              style={{ position: "absolute", top: 7, right: 10 }}
+              style={{ top: 10, right: 10 }}
             >
               <Image
                 source={require("../../assets/touch-id.png")}
@@ -84,23 +57,6 @@ function LoginScreen() {
           )}
         </View>
       </View>
-
-      <TouchableOpacity
-        onPress={() => handleInputLogin()}
-        style={{
-          padding: 15,
-          width: "70%",
-          backgroundColor: "#009688",
-          borderRadius: 3,
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 40,
-        }}
-      >
-        <Text style={{ color: "#fff", fontSize: 16, fontWeight: "500" }}>
-          Log in
-        </Text>
-      </TouchableOpacity>
     </View>
   );
 }
